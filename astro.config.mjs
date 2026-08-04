fc2f5bc474dcc8fa5f44c10eb65ea55ea49f8525
Neetify0 <240364908+Neetify0@users.noreply.github.com>
2026-08-03 23:23:13 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   trailingSlash: "never",
   site: "https://shadowcrystal.org",
-  base: "/website",
+  base: "/website/",
   integrations: [sitemap()],
 });
